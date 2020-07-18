b6ea1d2a281c2d428c97ddc9e2254af95ab00b2b
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-18 11:42:54 +0100

    Update basic k-mer search.py
    --> fixed variable name error ('k' was undefined -  replaced it with len(kmer)

--- basic k-mer search.py
+++ basic k-mer search.py
@@ -4,7 +4,7 @@ def basic_kmer_search (genome, kmer):
   count = 0
   for i in range (len(genome)-len(kmer)+1):
 # genome of length L has (L-k+1) k-mers
-    if genome[i:i+k]==kmer:
+    if genome[i:i+len(kmer)]==kmer:
       count += 1
   print (count)
   return;
