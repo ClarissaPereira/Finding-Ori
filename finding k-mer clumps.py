16debba32afbc41832e14307baf78fe1c85ac39b
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-18 21:54:46 +0100

    Update finding k-mer clumps.py
    --> Renamed variables for additional clarity
    --> Added helpful comments
    --> Converted code into a function
    --> Redefined a variable

--- finding k-mer clumps.py
+++ finding k-mer clumps.py
@@ -1,31 +1,34 @@
-genome = 'CGGACTCGACAGATGTGAAGAACGACAATGTGAAGACTCGACACGACAGAGTGAAGAGAAGAGGAAACATTGTAA'
-k = 5
-t = 4
-frequent = {}
-j = []
-counts = {}
-position = {}
-num = []
-for i in range (len(genome)):
-    kmer = genome[i:i + k]
-    if kmer in counts:
-        counts[kmer] += 1
-        position.setdefault(kmer, []).append(i)
-    else:
-        counts[kmer] = 1
-        position.setdefault(kmer, []).append(i)
-print (counts)
-for k in counts:
-  	if counts[k] >= t:
-          frequent[k] = counts[k]
-          #print (counts[k])
-print (frequent)
-print (position)
-for j in frequent:
-     if j in position:
-         comparisonlist = position[j]
-         minimum = min(comparisonlist)
-         maximum = max(comparisonlist)
-         if (maximum - minimum) <= 50:
-             num.append(j)
-print (num)
+# Find frequent k-mers that cluster together into clumps in the genome
+# inputs are:    genome 
+#                k (the length of the k-mer) 
+#                L (the maximum length of a clump region)
+#                t (the minimum frequency of a k-mer in the clump)  
+def kmer_clump_finder (genome, k, t, L):
+    frequent_kmers = {}
+    kmer_count = {}
+    kmer_position = {}
+    clump_forming_kmers = []
+    for i in range (len(genome)):
+        kmer = genome[i:i + k]
+        if kmer in kmer_count:
+            kmer_count[kmer] += 1
+            kmer_position.setdefault(kmer, []).append(i)
+        else:
+            kmer_count[kmer] = 1
+            kmer_position.setdefault(kmer, []).append(i)
+    for k in kmer_count:
+  	    if kmer_count[k] >= t:
+            frequent_kmers[k] = kmer_count[k]
+    for j in frequent_kmers:
+        if j in kmer_position:
+            position_range = kmer_position[j]
+            minimum = min(position_range)
+            maximum = max(position_range)
+            if (maximum - minimum) <= L:
+                clump_forming_kmers.append(j)
+# checks if all positions of the frequent k-mer cluster within a clump region of maximum length L 
+    print (clump_forming_kmers)
+    return;
+
+kmer_clump_finder(genome = 'CGGACTCGACAGATGTGAAGAACGA', k=3, t=2, L=10) 
+# Output --> ['GAC', 'AGA', 'GAA']
