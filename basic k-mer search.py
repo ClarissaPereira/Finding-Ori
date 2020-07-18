192c53f65ee6cb7bd0dfd51614dd6bf99bbd9be7
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-18 11:36:52 +0100

    Update basic k-mer search.py
    --> Converted code into a function
    --> Added comments to explain the purpose of the function

--- basic k-mer search.py
+++ basic k-mer search.py
@@ -1,7 +1,12 @@
-genome = ''
-kmer = ''
-count = 0
-for i in range (len(genome)-len(kmer)+1):
-  if genome[i:i+k]==kmer:
-    count += 1
-print (count)
+# searches for a specific k-mer string within a larger genome
+# accounts for overlapping sub-strings
+def basic_kmer_search (genome, kmer):
+  count = 0
+  for i in range (len(genome)-len(kmer)+1):
+# genome of length L has (L-k+1) k-mers
+    if genome[i:i+k]==kmer:
+      count += 1
+  print (count)
+  return;
+
+basic_kmer_search (genome = 'AGAGAT', kmer = 'AGA')
