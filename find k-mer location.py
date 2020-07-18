cc02a92c928d096c2ab4e244029bc4a867cf8676
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-18 12:38:22 +0100

    Update find k-mer location.py

--- find k-mer location.py
+++ find k-mer location.py
@@ -1,1 +1,11 @@
 # Given both the genome and the specific k-mer string, this function find the locations within the genome where the k-mer appears
+def locate_kmer (genome, kmer): 
+  location_list = []
+  for i in range (len(genome)-len(kmer)+1):
+    if genome[i:i+len(kmer)] == kmer:
+      location_list.append(i)
+  print (location_list, sep = ' ')
+  return;
+
+locate_kmer (genome = 'AGCATCATG', kmer = 'CAT')
+# Output --> [2, 5]
