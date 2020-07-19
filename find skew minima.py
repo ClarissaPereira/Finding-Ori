6325d184ab091ce17dd374046e403d4a0df0dd7c
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-19 13:31:47 +0100

    Update find skew minima.py

--- find skew minima.py
+++ find skew minima.py
@@ -1,1 +1,22 @@
 def skew_minima (genome):
+  skew = 0
+  skew_dict = {0:0}
+  minima = []
+  for i in range (len(genome)):
+    if genome[i]=='C':
+      skew = skew - 1
+      skew_dict[i] = skew
+    elif genome[i]=='G':
+      skew = skew + 1
+      skew_dict[i] = skew
+    else:
+      skew_dict[i] = skew
+  minimum_skew = min(skew_dict.values())
+  for j in skew_dict:
+    if skew_dict[j] == minimum_skew:
+      minima.append(j)
+  print (minima)
+  return;
+
+skew_minima (genome = 'ATAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT')
+# Output --> [11, 24]             
