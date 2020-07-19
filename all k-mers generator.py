af117bb3a938df56870837bfa15c1f9bf698f556
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-19 16:51:37 +0100

    Update all k-mers generator.py

--- all k-mers generator.py
+++ all k-mers generator.py
@@ -1,3 +1,12 @@
 # a Cartesian Product function which generates all possible k-mers when given k 
 
-def a
+def all_kmers (k):
+  import itertools
+  bases = 'AGCT'
+  kmers = itertools.product(bases, repeat = 5)
+  kmer_list = list(''.join(w) for w in list(kmers))
+  print (kmer_list)
+  return;
+
+all_kmers(k = 3)
+# Output -> ['AAA', 'AAC', 'AAG', ..., 'TTG', 'TTT']
