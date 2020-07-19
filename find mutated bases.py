42c78b34b4b58664c5efece321205c13fad3c623
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-19 14:13:31 +0100

    Create find mutated bases.py

---
+++ find mutated bases.py
@@ -0,0 +1,2 @@
+# Because of deamination mutations, some DnaA boxes will have one changed nucleotide but DnaA protein can stil bind to variations of the DnaA box sequence
+# This function identifies the number of mutations between an original and replicated strand of DNA by calculating Hamming distance
