7a24744d6908df40aafbc7f813859e34930f774e
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-19 11:32:24 +0100

    Update reverse complement generator.py

--- reverse complement generator.py
+++ reverse complement generator.py
@@ -1,1 +1,15 @@
-def generate_reverse_complement(genome):
+def reverse_complement(genome):
+	genome = genome.casefold()
+	new_genome = genome.replace ("a", "T")
+	genome = new_genome
+	new_genome = genome.replace ("g", "C")
+	genome = new_genome
+	new_genome = genome.replace ("c", "G")
+	genome = new_genome
+	new_genome = genome.replace ("t", "A")
+	genome = new_genome
+	print (genome[::-1])
+	return;
+
+reverse_complement(genome = 'AAAACCCGGT')
+# Output --> 'ACCGGGTTTT'
