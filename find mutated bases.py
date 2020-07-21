593627b46eae6fb479d59c75c120ead7e2b8e5cd
ClarissaPereira <68158694+ClarissaPereira@users.noreply.github.com>
2020-07-21 14:15:42 +0100

    Update find mutated bases.py
    Increase efficiency and readability by: 
    --> zipping sequence 1 and sequence 2 into a tuple
    --> using the sum function instead of incrementing hamming iteratively

--- find mutated bases.py
+++ find mutated bases.py
@@ -11,3 +11,9 @@ def hamming_distance (sequence1, sequence2):
 
 hamming_distance (sequence1 = 'AGCTATAGC', sequence2 = 'AGCTGTGAC')
 # Output -> 3
+
+# ALT # 
+# Can be made more efficient by zipping sequence 1 and sequence 2 into a tuple
+# And more readable by using the sum function instead of incrementing hamming iteratively 
+def hamming_distance(pattern, seq):
+    return sum(pattern_base1 != pattern_base2 for pattern_base1, pattern_base2 in zip(pattern, seq))
